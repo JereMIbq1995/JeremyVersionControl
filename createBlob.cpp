e1b51fd1a5ad63a28377d440a551e88f236d773d
Jeremy Duong <jeremibq@gmail.com>
2022-05-25 19:59:05 -0600

    simplified code by calling BlobCreator class

--- createBlob.cpp
+++ createBlob.cpp
@@ -2,65 +2,8 @@
 #include <string>
 #include <fstream>
 #include "HashingUnit.h"
+#include "BlobCreator.h"
 using namespace std;
-
-void createBlob(string fileName, string blobName, char objectType) {
-    
-    fstream fout;
-    fstream fin;
-
-    fout.open(".jvc/obj/blob/" + blobName, ios::out | ios::binary);
-    fin.open(fileName, ios::in | ios::binary);
-    
-    char* objType = (char*)&objectType;
-    // cout << objType[0] << endl;
-    if (fout.is_open() && fin.is_open()) {
-        fout.write(objType, 1);
-        char* oneByte;
-        while (fin.read(oneByte, 1)) {
-            fout.write(oneByte, 1);
-        }
-    }
-    else {
-        cout << "File does not exist or failed to open blob" << "\n";
-    }
-    fout.close();
-    fin.close();
-}
-
-void createFileFromBlob(string blobName, string fileName)
-{
-
-    fstream fout;
-    fstream fin;
-
-    fin.open(".jvc/obj/blob/" + blobName, ios::in | ios::binary);
-    fout.open(fileName, ios::out | ios::binary);
-
-    if (fout.is_open() && fin.is_open())
-    {
-        char objType[2];
-        fin.read(objType, 1);
-        if (objType[0] == 'b') {
-            cout << "Object Type: BLOB" << "\n";
-        }
-
-        cout << "Writing to file " << fileName << " from blob " << blobName << endl;
-        char c[1];
-        
-        while (fin.read(c, 1))
-        {
-            fout.write(c, 1);
-        }
-    }
-    else
-    {
-        cout << "Blob does not exist or failed to open file"
-             << "\n";
-    }
-    fout.close();
-    fin.close();
-}
 
 /*
     Helper function: return the copy file name
@@ -94,6 +37,7 @@ string getCopyFileName(string fileName) {
 int main() {
     // Initialize hashing unit
     HashingUnit hashingUnit;
+    BlobCreator blobCreator;
     
     // Get the fileName of the file the user would like to hash
     string fileName;
@@ -103,9 +47,8 @@ int main() {
     string fileDigest = hashingUnit.hashFileContent(fileName);
     cout << "Hashed content of '" << fileName << "' is: " << fileDigest << "\n";
 
-    char objType = 'b';
-    createBlob(fileName, fileDigest, objType);
+    blobCreator.createBlob(fileName);
 
     string copyFileName = getCopyFileName(fileName);
-    createFileFromBlob(fileDigest, copyFileName);
+    blobCreator.createFileFromBlob(fileDigest, copyFileName);
 } 
