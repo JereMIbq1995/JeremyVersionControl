29b550712efae09bf83cd98a0b342f144e2fc6c0
Jeremy Duong <jeremibq@gmail.com>
2022-06-10 20:41:53 -0600

    Simplified jvcStatus by encapsulating a few functionalities and data struct

--- jvcStatus.cpp
+++ jvcStatus.cpp
@@ -6,12 +6,12 @@
 #include <vector>
 #include <set>
 #include <map>
+#include "FileEntry.h"
+#include "ObjectsReader.h"
 using namespace std;
 
-struct FileEntry {
-    string type;
-    string codeName;
-};
+// This should be a member variable of the functionality class once completed
+ObjectReader objReader;
 
 void traverseAll(filesystem::path currentPath, vector<string> &newFiles, const set<string> &ignores) {
 
@@ -29,70 +29,12 @@ void traverseAll(filesystem::path currentPath, vector<string> &newFiles, const s
     }
 }
 
-void getTreeEntries(map<string, FileEntry> &entries, string treeName) {
-    fstream fin;
-    fin.open(".jvc/obj/tree/" + treeName);
-    string entryName;
-    FileEntry entry;
-
-    if (fin.is_open())
-    {
-        while (fin >> entry.type) {
-            fin >> entry.codeName;
-            fin >> entryName;
-
-            entries.insert(pair(entryName, entry));
-        }
-    }
-    else {
-        cout << "Could not open file " << treeName << endl;
-    }
-    fin.close();
-}
-
-bool diff(string filePath, string blobName) {
-
-    ifstream fin1;
-    ifstream fin2;
-
-    fin1.open(filePath, ios::in | ios::binary);
-    fin2.open(".jvc/obj/blob/" + blobName, ios::in | ios::binary);
-
-    char c1 = ' ';
-    char c2 = ' ';
-
-    fin1.seekg(0, fin1.end);
-    fin2.seekg(0, fin2.end);
-    int size1 = fin1.tellg();
-    int size2 = fin2.tellg();
-    // cout << "size1 - size2:  " << size1 << " - " << size2 << endl;
-
-    if (size1 != size2)
-    {
-        return true;
-    }
-    else
-    {
-        fin1.seekg(0, fin1.beg);
-        fin2.seekg(0, fin2.beg);
-
-        while (fin1.get(c1) && fin2.get(c2))
-        {
-            if (c1 != c2)
-            {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
 void traverseTreeWithDir(filesystem::path currentPath, string treeName, vector<string> &newFiles, vector<string> &deletedFiles,
                          vector<string> &modifiedFiles, const set<string> &ignores)
 {
     // Get all the fileNames and foldernames mentioned in the current tree
     map<string, FileEntry> entries;
-    getTreeEntries(entries, treeName);
+    objReader.getTreeEntries(entries, treeName);
 
     // TODO: Implement this...
     filesystem::directory_iterator it(currentPath);
@@ -121,7 +63,7 @@ void traverseTreeWithDir(filesystem::path currentPath, string treeName, vector<s
                 // If it's a file, compare content, if different, add to MODIFIED
                 else
                 {
-                    if (diff(dir_entry.path().u8string(), matchPos->second.codeName))
+                    if (objReader.diff(dir_entry.path().u8string(), matchPos->second.codeName))
                     {
                         modifiedFiles.push_back(entry_path.u8string());
                     }
@@ -207,19 +149,6 @@ void getStatus(filesystem::path currentPath, vector<string> &newFiles, vector<st
     }
 }
 
-void getIgnores(set<string> &ignores) {
-    if (filesystem::exists("./.jvcIgnore")) {
-        ifstream fin;
-        fin.open("./.jvcIgnore");
-        
-        string fileName;
-        while (getline(fin, fileName)) {
-            ignores.insert(fileName);
-        }
-        fin.close();
-    }
-}
-
 void displayResult(const vector<string> &newFiles, const vector<string> &deletedFiles, const vector<string> &modifiedFiles) {
     
     int nSize = newFiles.size();
@@ -268,7 +197,7 @@ int main()
 
     // Prepare the ignores set
     set<string> ignores;
-    getIgnores(ignores);
+    objReader.getIgnores(ignores);
 
     // Meat and butter of the functionality
     getStatus(".\\", newFiles, deletedFiles, modifiedFiles, ignores);
